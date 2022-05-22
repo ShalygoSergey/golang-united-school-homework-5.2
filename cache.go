b7e4428e86257cce4abb805201d119bdfee8e13d
Шалыго Сергей Михайлович <Shalygo.S.Mi@sberbank.ru>
2022-05-22 11:09:35 +0300

    первая попытка

--- cache.go
+++ cache.go
@@ -2,22 +2,67 @@ package cache
 
 import "time"
 
+type Value struct {
+	value    string
+	deadline *time.Time
+}
+
+func NewValue(value string, deadline *time.Time) Value {
+	return Value{
+		value:    value,
+		deadline: deadline,
+	}
+}
+
 type Cache struct {
+	m map[string]Value
 }
 
 func NewCache() Cache {
-	return Cache{}
+	c := Cache{}
+	c.m = make(map[string]Value)
+	return c
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (c Cache) Get(key string) (string, bool) {
 
+	value := ""
+	exist := false
+
+	v, ok := c.m[key]
+
+	if ok {
+		if v.deadline != nil && v.deadline.Before(time.Now()) {
+			delete(c.m, key)
+		} else {
+			value = v.value
+			exist = true
+		}
+	}
+
+	return value, exist
 }
 
-func (receiver) Put(key, value string) {
+func (c Cache) Put(key, value string) {
+	c.m[key] = NewValue(value, nil)
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
+
+	var keys []string
+
+	now := time.Now()
+	for k, v := range c.m {
+		if v.deadline != nil && v.deadline.Before(now) {
+			delete(c.m, k)
+		} else {
+			keys = append(keys, k)
+		}
+	}
+
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c Cache) PutTill(key, value string, deadline time.Time) {
+	c.m[key] = NewValue(value, &deadline)
 }
